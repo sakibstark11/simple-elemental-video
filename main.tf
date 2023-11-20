bb94706b390697811efea3256c82cc026a901892
melon <m.melon@m2amedia.tv>
2023-11-20 13:21:14 +0600

    fix main module

--- main.tf
+++ main.tf
@@ -2,7 +2,12 @@ data "http" "current_ip" {
   url = "https://api.ipify.org"
 }
 
-module "orchestrate" {
+module "orchestrator" {
   source = "./terraform"
-  a = "${data.http.current_ip.response_body}/32"
+  prefix = "simple-eml"
+  mediaconnect_settings = {
+    mediaconnect_protocol = "srt-listener"
+    whitelist_cidr_address = ["${data.http.current_ip.response_body}/32"]
+    ingress_port = 5000
+  }
 }
