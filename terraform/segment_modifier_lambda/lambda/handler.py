b1d261e87fac7a51c0357a7af0722e18c1af9c85
tasnuvaleeya <tasnuva2606@gmail.com>
2024-05-01 14:28:41 +0600

    put text to detected face, optimized processor function

--- terraform/segment_modifier_lambda/lambda/handler.py
+++ terraform/segment_modifier_lambda/lambda/handler.py
@@ -1,8 +1,73 @@
 import json
+import cv2
+import face_recognition
+import asyncio
 import os
 
 import boto3
 from postStreamToMediaPackage import postStreamToMediaPackage, postStreamToMultipleMediaPackageEndpoints
+
+
+def detect_bounding_box(frame):
+    print('detecting bounding box')
+    rgb_frame = frame[:, :, ::-1]
+    face_locations = face_recognition.face_locations(rgb_frame)
+    for top, right, bottom, left in face_locations:
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 255), 2)
+        face_image = frame[top:bottom, left:right]
+        blurred_face = cv2.blur(face_image, (99, 99))
+        frame[top:bottom, left:right] = blurred_face
+    return face_locations
+
+
+def process_video_stream(video_stream):
+    print('processing video stream', video_stream)
+    cap = cv2.VideoCapture(video_stream)
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    # out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+
+    if not cap.isOpened():
+        print("Error: Unable to open video stream")
+        return
+    frames = []
+    while True:
+        result, frame = cap.read()
+
+        if result is False:
+            break
+
+        boxes = detect_bounding_box(frame)
+        # out.write(frame)
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    bytes_list = []
+    for frame in frames:
+        result, encimg = cv2.imencode('.jpg', frame, encode_param)
+        if result:
+            bytes_list.append(encimg.tobytes())
+
+    os.remove(video_stream)
+
+    return bytes_list
+
+
+async def async_handler(fileContent):
+    video_path = '/tmp/video.ts'
+    with open(video_path, 'wb') as f:
+        f.write(fileContent)
+    process_video_stream(video_path)
+    await asyncio.sleep(1)
+
+    return 'success'
+
 
 def getS3FileDetailsFromEvent(event):
     return {
@@ -13,27 +78,34 @@ def getS3FileDetailsFromEvent(event):
 
 def handler(event, context):
     s3 = boto3.client("s3")
-
     s3FileDetails = getS3FileDetailsFromEvent(event)
+    fileName = s3FileDetails["fileName"]
+
     fileObject = s3.get_object(
         Bucket=s3FileDetails["bucketName"], Key=s3FileDetails["key"])
     fileContent = fileObject["Body"].read()
+
+    if fileName.endswith('.m3u8'):
+        content = fileContent
+    else:
+        content = asyncio.run(async_handler(fileContent))
 
     envVariablesList = os.getenv("mediapackage_hls_ingest_endpoints")
 
     # ingestResult = [
     #     postStreamToMediaPackage(
-    #         envVars, s3FileDetails["fileName"], 
-    #         fileContent, 
+    #         envVars, s3FileDetails["fileName"],
+    #         fileContent,
     #         { "contentLength": fileObject['ContentLength'], "contentType": "application/octet-stream" }
     #     ) for envVars in json.loads(envVariablesList)
     # ]
     ingestResult = postStreamToMultipleMediaPackageEndpoints(
         json.loads(envVariablesList),
-        s3FileDetails["fileName"], 
-        fileContent, 
-        { "contentLength": fileObject['ContentLength'], "contentType": "application/octet-stream" }, 
+        fileName,
+        content,
+        { "contentLength": fileObject['ContentLength'], "contentType": "application/octet-stream" },
     )
-    
+    #
     print(ingestResult)
     return ingestResult
+handler({}, {})
--- terraform/segment_modifier_lambda/lambda/image-processor.py
+++ terraform/segment_modifier_lambda/lambda/image-processor.py
@@ -6,23 +6,70 @@ import boto3
 import os
 
 
-async def async_handler(event, context, fileObject):
-    print('fileObject ', fileObject)
+def detect_bounding_box(frame):
+    print('detecting bounding box')
+    rgb_frame = frame[:, :, ::-1]
+    face_locations = face_recognition.face_locations(rgb_frame)
+    for top, right, bottom, left in face_locations:
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 255), 2)
+        cv2.putText(frame, 'Face Detected', (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+
+        face_image = frame[top:bottom, left:right]
+        blurred_face = cv2.blur(face_image, (99, 99))
+        frame[top:bottom, left:right] = blurred_face
+    return face_locations
+
+
+async def process_video_stream(video_stream):
+    print('processing video stream', video_stream)
+    cap = cv2.VideoCapture(video_stream)
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
+
+    if not cap.isOpened():
+        print("Error: Unable to open video stream")
+        return []
+
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    bytes_list = []
+
+    while True:
+        result, frame = cap.read()
+
+        if not result:
+            break
+        faces = detect_bounding_box(frame)
+        out.write(frame)
+
+        # result, encimg = cv2.imencode('.jpg', frame, encode_param)
+
+        if result:
+            bytes_list.append(frame.tobytes())
+
+    cap.release()
+    out.release()
+    return bytes_list
+
+
+async def async_handler(fileObject):
+    print('async handler')
     fileContent = fileObject["Body"].read()
     video_path = 'video.ts'
     with open(video_path, 'wb') as f:
         f.write(fileContent)
-    process_video_stream(video_path)
-    await asyncio.sleep(1)
+    byte_list = await process_video_stream(video_path)
 
-    return 'Success'
+    os.remove(video_path)
+    return byte_list
 
 
-def lambda_handler(event, context):
+def lambda_handler():
+    print('Loading function')
     s3 = boto3.client("s3")
     fileObject = s3.get_object(
-        Bucket="bucket-name", Key='object-key')
-    result = asyncio.run(async_handler(event, context, fileObject))
+        Bucket="tasnuva-zaman-segment-storage", Key='data01.ts')
+    result = asyncio.run(async_handler(fileObject))
+    print('result ', result)
 
     return {
         'statusCode': 200,
@@ -30,40 +77,4 @@ def lambda_handler(event, context):
     }
 
 
-lambda_handler({}, {})
-
-
-def detect_bounding_box(frame):
-    print('detecting bounding box')
-    rgb_frame = frame[:, :, ::-1]
-    face_locations = face_recognition.face_locations(rgb_frame)
-    for top, right, bottom, left in face_locations:
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 255), 2)
-        face_image = frame[top:bottom, left:right]
-        blurred_face = cv2.blur(face_image, (99, 99))
-        frame[top:bottom, left:right] = blurred_face
-    return face_locations
-
-
-def process_video_stream(video_stream):
-    print('processing video stream', video_stream)
-    cap = cv2.VideoCapture(video_stream)
-
-    if not cap.isOpened():
-        print("Error: Unable to open video stream")
-        return
-
-    while True:
-        result, frame = cap.read()
-        if result is False:
-            break
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        faces = detect_bounding_box(small_frame)
-
-        cv2.imshow('Video', small_frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-    os.remove(video_stream)
+lambda_handler()
