f94663d29da672ad1ca5e9f4ca028121b2dd3b76
sakibstark11 <16sakib@gmail.com>
2024-05-06 14:48:59 +0100

    wip

--- terraform/segment_modifier_lambda/docker.tf
+++ terraform/segment_modifier_lambda/docker.tf
@@ -2,22 +2,26 @@ resource "aws_ecr_repository" "ecr_repo" {
   name = "${var.prefix}-python-container-image"
 }
 
+locals {
+  dockerfile_md5 = filemd5("${path.module}/Lambda.Dockerfile")
+}
+
 resource "docker_image" "ecr_image" {
   name = "${var.prefix}-ecr-image"
 
   triggers = {
-    dockerfile_md5 = filemd5("${path.module}/Dockerfile")
+    dockerfile_md5 = local.dockerfile_md5
   }
   build {
     context      = abspath("${path.module}/")
     force_remove = true
-    dockerfile   = "Dockerfile"
+    dockerfile   = "Lambda.Dockerfile"
   }
 }
 
 resource "docker_tag" "ecr_tag" {
   source_image = docker_image.ecr_image.name
-  target_image = "${aws_ecr_repository.ecr_repo.repository_url}:${filemd5("${path.module}/Dockerfile")}"
+  target_image = "${aws_ecr_repository.ecr_repo.repository_url}:${local.dockerfile_md5}"
 }
 
 resource "docker_registry_image" "container_image" {
